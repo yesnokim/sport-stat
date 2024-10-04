7d4853e18a46e5eceb4e40dae9391d24d3fff64c
ChrisKim <yesnokim@gmail.com>
2024-10-05 00:10:09 +0900

    change to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Home from './Home';
 import ss from "./App.module.scss"
 
 function App() {
   return (
     <div className={ss.bg}>
-      <BrowserRouter>
+      <HashRouter>
         <Home />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
