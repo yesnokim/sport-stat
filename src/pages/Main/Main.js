5f41ccbbc8fef5206d081743937e1fac51e5de9a
ChrisKim <yesnokim@gmail.com>
2024-10-06 03:01:31 +0900

    add signout button

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,8 +1,11 @@
 import MatchList from "../../components/MatchList/MatchList";
+import SignOutButton from "../../components/SignOutButton/SignOutButton";
+import ss from "./Main.module.scss"
 
 const Main = () => {
 
-    return <div><MatchList /></div>
+    return <div className={ss.bg}>
+        <div className={ss.header}><SignOutButton /></div><MatchList /></div>
 }
 
 export default Main;
