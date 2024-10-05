a0a89cfecd80bd559456719a82e369abbc849358
ChrisKim <yesnokim@gmail.com>
2024-10-06 02:15:53 +0900

    use searchParmas

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -7,6 +7,19 @@ export const formatDate = (timestamp) => {
     return `${year}-${month}-${day}`; // 'YYYY-MM-DD' 형식으로 반환
 };
 
+export const formatDateDatetimeLocal = (date) => {
+    if (!date) return '';
+    
+    const d = new Date(date);
+    const year = d.getFullYear();
+    const month = String(d.getMonth() + 1).padStart(2, '0');  // 월은 0부터 시작하므로 +1
+    const day = String(d.getDate()).padStart(2, '0');
+    const hours = String(d.getHours()).padStart(2, '0');
+    const minutes = String(d.getMinutes()).padStart(2, '0');
+
+    return `${year}-${month}-${day}T${hours}:${minutes}`;  // 'YYYY-MM-DDTHH:MM' 형식 반환
+};
+
 export const getPlayStat = (state) => {
     const passSuccess = state["forwardPass"] + state["sidePass"] + state["backPass"];
     const passTries = passSuccess + state["failedPass"];
