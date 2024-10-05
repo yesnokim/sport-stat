1ec56b6d83889d76f867efdd19c70636ac682fcb
ChrisKim <yesnokim@gmail.com>
2024-10-05 23:04:33 +0900

    add firebase env

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+// firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Firebase 프로젝트 설정 (Firebase 콘솔에서 복사)
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  };
+
+// Firebase 초기화
+const app = initializeApp(firebaseConfig);
+
+// Firestore 인스턴스 가져오기
+const db = getFirestore(app);
+
+export { db };
